e56834dc728525589f2f0149a85fa2d1ff7b2605
Cedar DeLacy <seed@tsck.agency>
2023-01-10 16:55:19 -0700

    so close to fimishing the theme thing

--- 5.0/changeTheme.js
+++ 5.0/changeTheme.js
@@ -1,14 +1,14 @@
-
+var theme = 0;
 function changeTheme() {
-let root = document.documentElement;
-var theme = 'dark';
-if (theme = 'dark') {
-    root.style.setProperty('--text-color', 'black');
-    root.style.setProperty('--panel-color', 'rgba(255, 255, 255, 0.5');
-    theme = 'light';
-} else if (theme = 'light') {
-    root.style.setProperty('--text-color', 'white');
-    root.style.setProperty('--panel-color', 'rgba(0, 0, 0, 0.5');
-    theme = 'dark';
-}
-}
+    let root = document.documentElement;
+    if (theme === 0) {
+        root.style.setProperty('--text-color', 'black');
+        root.style.setProperty('--panel-color', 'rgba(255, 255, 255, 0.5');
+        root.style.setProperty('--background-color', 'white');
+        theme = 1
+    } else if (theme === 1) {
+        root.style.setProperty('--text-color', 'white');
+        root.style.setProperty('--panel-color', 'rgba(0, 0, 0, 0.5');
+        root.style.setProperty('--background-color', '#111');
+        theme = 0
+}}
